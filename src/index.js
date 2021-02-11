b52ee33166897d028492682b1f67be3ded08a7e3
AlvinAhmadov <alvin.dev.ahmadov@gmail.com>
2021-02-11 14:00:10 +0400

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import {ProcessorType8} from "./recognizers.js";
 
 async function recognize() {
-	let proc = new ProcessorType8('params.yaml')
+	let proc = new ProcessorType8('params.yaml', true);
+	let text = '';
 	
 	try {
-		return proc.predict('./models/example8.png');
+		text = await proc.predict('./models/example8.png');
 	} catch (e) {
-		console.error(e)
+		console.error(e);
 	}
+	return text;
 }
 
 recognize().then(
