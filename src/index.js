3c381a7b91bc5b48d82141087c0bebc633f615ad
AlvinAhmadov <alvin.dev.ahmadov@gmail.com>
2021-02-12 13:33:44 +0400

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import {ProcessorType8} from "./recognizers.js";
+import {DATA_ROOT}      from "./constants.js"
+import {pathJoin}       from "./utils.js";
 
-async function recognize(config_path, image_path) {
+async function recognize(config_path, image_path, shardsPrefix) {
 	let proc = new ProcessorType8(config_path, false);
-	let text = '';
-	
-	try {
-		text = await proc.predict(image_path);
-	} catch (e) {
-		console.error(e);
-	}
-	return text;
+	return await proc.predict(image_path, shardsPrefix);
 }
 
-recognize('./models/params.yaml',
-          './models/example8.png')
-	.then(text => console.log("Text:", text));
+recognize('params.yaml',
+          pathJoin(DATA_ROOT, "example8.png"),
+          'data/models/type8')
+	.then(t => console.log("Text:", t))
+	.catch(reason => console.error(reason));
